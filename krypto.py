78cbc9e2840bbac358d050f3bf6f52697f0277b0
I-M Siegel <mea.siegel@gmail.com>
2021-10-08 19:33:20 -0700

    (1) Added print_numbers and print_operator_key functions\n(2) Added global ACCEPTED_OPERATORS list\n(3) Changed variable names from operation to operator\n(4) Fixed back-to-back parentheses bug\n(5) Added exponent, radical and factorial operations\n(6) Returns empty lists and strings instead of False from functions that otherwise return lists or strings

--- krypto.py
+++ krypto.py
@@ -1,7 +1,6 @@
 """
-    CS 5001, Fall 2021
-    Lab 4-B
-    Ilana-Mahmea Siegel
+    Created by I-M Siegel
+    Updated 10-8-21
 
     Facilitates a Krypto game/puzzle.
 
@@ -15,6 +14,9 @@
     proposed solutions. When the user enters 'quit', the program prints
     any correct solutions that the user entered.
 """
+
+# Acceptable operators
+ACCEPTED_OPERATORS = ["*", "/", "+", "-", "!", "^", "v"]
 
 
 def main():
@@ -32,9 +34,7 @@ def main():
         numbers, target = get_numbers(sd)
 
         # Print numbers for user
-        print("\nNumbers: {}, {}, {}, {}, {}.\nTarget: {}"
-              .format(numbers[0], numbers[1], numbers[2], numbers[3],
-                      numbers[4], target))
+        print_numbers(numbers, target)
 
         # Get user's solutions and print them (if they exist) when user quits
         solutions = get_solutions(numbers, target)
@@ -55,8 +55,9 @@ def print_instructions():
     print("I'll give you five random numbers between 1 and 25.")
     print("You need to figure out how to use them to get to"
           " a sixth random number, the 'target.'\n")
-    print("You can use multiplication (*), division (/),"
-          " addition (+) and subtraction (-).")
+    print("You can use multiplication, division, addition, subtraction, "
+          "exponentiation, radicals, and factorials:\n")
+    print_operator_key()
     print("You must use all five numbers, and you cannot repeat them.\n")
     print("Enter each solution you think you've come up with.\n")
     print("Enter 'q' or 'quit' at any time to quit.\n")
@@ -113,6 +114,20 @@ def get_numbers(sd):
     return numbers, target
 
 
+def print_numbers(numbers, target):
+    """
+        Prints elements 0-4 from numbers and prints target.
+
+        Input:
+            (list) numbers: list of 5 operand numbers.
+            (int) target: target number
+    """
+
+    print("\nNumbers: {}, {}, {}, {}, {}.\nTarget: {}"
+          .format(numbers[0], numbers[1], numbers[2], numbers[3], numbers[4],
+                  target))
+
+
 def get_solutions(numbers, target):
     """
         Prompt user to enter solutions until they enter 'q' or 'quit'.
@@ -148,6 +163,7 @@ def get_solutions(numbers, target):
             print("Try again.\n")
 
         # Next attempt
+        print_numbers(numbers, target)
         attempt = input("Enter solution: ")
 
     # Contains all of user's correct solutions. Empty if no solutions entered.
@@ -198,22 +214,22 @@ def get_list_expression(attempt, numbers):
         of given expression.
 
         Returns False if attempt uses any numbers that aren't in numbers
-        list or if attempt uses operations that aren't in hard-coded
-        list (["*", "/", "+", "-"]).
+        list or if attempt uses operations that aren't in ACCEPTED_OPERATORS
+        list.
 
         Parameter: (str) attempt: Mathematical expression with integers,
-            +, -, *, /, (, ), or spaces.
+            +, -, *, /, !, ^, v, (, ), or spaces.
         Parameter: (int) target: Integer that attempt should evaluate to.
 
-        Return: (list) expression or (bool): list strings representing
-                numbers and operations from input string. Returns False if
-                a number in attempt is not in parameter numbers, if any
+        Return: (list): list of strings representing
+                numbers and operations from input string. Returns empty list
+                if a number in attempt is not in parameter numbers, if any
                 characters aren't numbers or in list of approved symbols,
                 or if there's the wrong number of operations.
     """
 
-    # To count operations in attempt. There should be 4 (for 5 numbers)
-    ops = 0
+    # Count numbers used
+    nums = 0
 
     # To hold each operand and operation in attempt
     expression = []
@@ -238,19 +254,17 @@ def get_list_expression(attempt, numbers):
                 if int(current_num_string) not in numbers:
                     print(current_num_string,
                           "isn't one of the given numbers.", end=' ')
-                    return False
+                    return []
 
                 else:
                     # Add to expression list
                     expression.append(current_num_string)
                     current_num_string = ""
+                    nums += 1
 
             # Add valid operations to expression list
-            if character in ["*", "/", "+", "-"]:
+            if character in ACCEPTED_OPERATORS:
                 expression.append(character)
-
-                # Count operations to confirm correct number
-                ops += 1
 
             # Not operations, but valid symbols to include
             elif character in ["(", ")"]:
@@ -261,7 +275,7 @@ def get_list_expression(attempt, numbers):
             # Anything else is an invalid symbols
             else:
                 print("'" + character + "' is an invalid symbol.", end=' ')
-                return False
+                return []
 
     # Need to append last number to list
     # Confirm current number is one of the given numbers
@@ -269,15 +283,25 @@ def get_list_expression(attempt, numbers):
         if int(current_num_string) not in numbers:
             print(current_num_string,
                   "isn't one of the given numbers.", end=' ')
-            return False
+            return []
         else:
             # Add to expression list
             expression.append(current_num_string)
+            nums += 1
 
-    # Confirm correct number of operations (and numbers, by extension)
-    if ops != 4:
-        print("Incorrect number of operations.", end=' ')
-        return False
+    # Confirm correct number of numbers used
+    if nums != 5:
+        print("Incorrect number of numbers.", end=' ')
+        return []
+
+    # Confirm all numbers used.
+    missing_nums = False
+    for n in numbers:
+        if str(n) not in expression:
+            print("Missing number {}.".format(n), end=' ')
+            missing_nums = True
+    if missing_nums:
+        return []
 
     return expression
 
@@ -289,8 +313,8 @@ def evaluate_expression(expression):
         Parameter: (list) expression: mathematical expression. Each element
                     is a string: integer, arithmetic operation or parentheses.
 
-        Return: (int) value or (bool) False: value of expression or False
-                if missing a close-parentheses.
+        Return: (int) value: value of expression or 0 if missing a
+                close-parentheses.
     """
 
     # Running value of expression
@@ -298,7 +322,7 @@ def evaluate_expression(expression):
 
     # The next operand and operation to perform
     next_operand = 0
-    next_operation = ""
+    next_operator = ""
 
     # Use a while loop so can conditionally change i's value as needed
     i = 0
@@ -312,8 +336,8 @@ def evaluate_expression(expression):
                 next_operand = int(expression[i])
 
         # Get operation to perform
-        elif expression[i] in ["*", "/", "+", "-"]:
-            next_operation = expression[i]
+        elif expression[i] in ACCEPTED_OPERATORS:
+            next_operator = expression[i]
 
         # Parentheses --
         # Evaluate expression inside parentheses before proceeding
@@ -333,28 +357,47 @@ def evaluate_expression(expression):
                 # Don't index beyond end of list
                 if j == len(expression):
                     print("Missing close-parenthesis.", end=' ')
-                    return False
+                    # Caller reads 0 as False, doesn't operate with it
+                    return 0
 
-            # Evaluate expression inside parentheses to be next operand
-            # Recursive
-            next_operand = evaluate_expression(expression[(i + 1):j])
+            # Recursively evaluate expression inside parentheses
+            if not value:
+                value = evaluate_expression(expression[(i + 1):j])
+            else:
+                next_operand = evaluate_expression(expression[(i + 1):j])
 
             # skip to other side of parentheses after evaluating val inside
             i = j
 
         # Evaluate new value based on next_operand and next_operation
-        if next_operand and next_operation:
-            value = operate(value, next_operation, next_operand)
+        # Factorial is a unary operation.
+        if (next_operand and next_operator) or (next_operator == "!"):
+            value = operate(value, next_operator, next_operand)
 
             # Reset next_operand and next_operation so don't enter this block
             # without getting a new next_operand and new next_operation.
+            # If operator was factorial, next_operand will already be 0
             next_operand = 0
-            next_operation = ""
+            next_operator = ""
 
         # Increment i to iterate through list
         i += 1
 
     return value
+
+
+def print_operator_key():
+    """
+        Prints menu of operations and their corresponding symbols.
+    """
+    print("*  Multiplication\n"
+          "/  Division\n"
+          "+  Addition\n"
+          "-  Subtraction\n"
+          "!  Factorial\n"
+          "^  Exponentiation\n"
+          "v  Root -- NOTE: Put the index AFTER the √ symbol,\n"
+          "           Example: for the cube root of 8, write '8 v 3'\n")
 
 
 def random_int(seed=7, max_num=25):
@@ -372,16 +415,20 @@ def random_int(seed=7, max_num=25):
         (int) num: pseudorandom number
     """
 
-    # Generate pseudorandom number.
+    # If max_num is 25, then seeding 14 returns 14, but other seeds can
+    # return 14, too.
+    if seed == 14:
+        seed = 13
+
     # Multiply seed by prime so generated numbers will work their way through
     # all ints between 1 and max_num
     # Add 1 after mod to shift range from [0 to (max_num - 1)]
     # to [1 to max_num]
-    num = ((17 * seed + 1) % max_num) + 1
+    num = ((17 * seed) % max_num) + 1
     return num
 
 
-def operate(operand_1, operation, operand_2):
+def operate(operand_1, operator, operand_2):
     """
         Performs given mathematical operation on given operands.
 
@@ -392,16 +439,48 @@ def operate(operand_1, operation, operand_2):
 
         Return: (int or float): result of performing operation
     """
-    # QUESTION: Is it better to have many possible returns like this or to
-    # do value *= next_operand, etc. and then return value at the end?
-    if operation == "*":
+
+    if operator == "*":
         return operand_1 * operand_2
-    if operation == "/":
+
+    if operator == "/":
         return operand_1 / operand_2
-    if operation == "+":
+
+    if operator == "+":
         return operand_1 + operand_2
-    if operation == "-":
+
+    if operator == "-":
         return operand_1 - operand_2
+
+    if operator == "!":
+        return factorial(operand_1)
+
+    if operator == "^":
+        return operand_1 ** operand_2
+
+    if operator == "v":
+        return operand_1 ** (1 / operand_2)
+
+
+def factorial(num):
+    """
+        Returns factorial of given number.
+        Returns 0 if num is negative or not an integer.
+    """
+    # Can't take factorial of negatives or non-integers.
+    # Running factorial() on a non-integer will eventually hit a negative val.
+    if num < 0:
+        print("Error: Can only take factorial of positive integers.")
+        # Error return 0 because outside expected range and evaluates to False
+        return 0
+
+    # Base case
+    if num == 0:
+        return 1
+
+    # Recursive call
+    else:
+        return num * factorial(num - 1)
 
 
 if __name__ == "__main__":
